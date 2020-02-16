b7bd3c7689dd0cf01ff69960263619f85f0e3928
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-02-16 21:19:08 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,8 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouse < ActiveRecord::Migration[5.1]
+  def create 
+    create_table :haunted_houses do |t|
+      t.string :name 
+      t.string :location 
+      t.string :theme 
+      t.
