74e09afcdbbcb73b756549f7a68fa1b91de482f2
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-02-16 21:22:59 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -10,6 +10,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.datetime :opening_date
       t.datetime :closing_date
       t.text :description
+      
+      t.timestamps null: false
     end 
   end 
   
