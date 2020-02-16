50e171b9940f17ffb77b97c4f5dae0a156abfab5
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-02-16 21:11:05 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,5 +6,7 @@ class CostumeStores < ActiveRecord::Migrate[5.1]
     create_table :costume_stores do |t|
       t.string :name 
       t.string :location
-      t.string :
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t. 
   end 
